e5f9da840f9f24c268a54a5c14de4f076bb6fd8e
Luan Maffra <maffra-oliveira@outlook.com>
2024-03-25 02:18:00 -0300

    test: adjusted jest file

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,14 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+export default {
   preset: 'ts-jest',
-  testEnvironment: 'node'
- };
+  bail: true,
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
+  clearMocks: true,
+  testEnvironment: 'node',
+  coverageProvider: 'v8',
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'html', 'lcov', 'text'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  detectOpenHandles: false
+}
